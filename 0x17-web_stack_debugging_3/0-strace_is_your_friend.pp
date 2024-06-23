743f10db1104930548604fd501e582927421ab9c
light1414 <dantata49@gmail.com>
2024-06-23 14:39:48 +0000

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Corrects bad `phpp` extensions files in the WordPress `.
 
-exec { 'fix-wordpress errors':
-  command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
