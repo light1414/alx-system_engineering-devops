9501e9125d7e546d5307444c952c2fcfa8b94f5a
light1414 <132365042+light1414@users.noreply.github.com>
2024-08-12 21:24:30 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Corrects bad `phpp` extensions files in the WordPress `.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
